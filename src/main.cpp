e11a50937f99c1299d0f1b7b931a25e85094049a
Mazda Bat <realmazdafan@gmail.com>
2025-12-06 19:12:35 -0700

    Add _cmake_ config and first cpp file.

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int, char**) {
+    std::cout << "Hello, from swift-cast!\n";
+    std::cout << "This is the second line of code.\n";
+    std::cout << "This is the third line of code.\n";
+}
