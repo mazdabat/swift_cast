6779929665654b5308817470cc97b62fc766c251
Mazda Bat <realmazdafan@gmail.com>
2025-12-06 22:44:23 -0700

    First version of signage.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_video.h>
+#include <stdlib.h>
+
+#include <filesystem>
 
 #define WIDTH 1280
 #define HEIGHT 720
@@ -18,11 +21,15 @@ int main(int, char**) {
         return 1;
     }  // Initialize SDL3
 
-    if (!SDL_CreateWindowAndRenderer("Hello swift-cast", 320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Hello swift-cast", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_FULLSCREEN,
+                                     &window, &renderer)) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create windows and renderer: %s", SDL_GetError());
     }
 
-    surface = SDL_LoadPNG("./pic_sc.png");
+    std::filesystem::path current_path = std::filesystem::current_path();
+    SDL_Log("Current path as string: %s", current_path.u8string().c_str());
+
+    surface = SDL_LoadPNG("res/pic_sc.png");
     if (!surface) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
     }
@@ -36,7 +43,10 @@ int main(int, char**) {
 
     while (1) {
         SDL_PollEvent(&event);
-        if (event.type == SDL_EVENT_QUIT) break;
+        if (event.type == SDL_EVENT_QUIT)
+            break;
+        else if (event.type == SDL_EVENT_KEY_DOWN)
+            break;
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
