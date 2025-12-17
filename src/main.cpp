a99bd9496b0949ae07d8912d03df0de5a429eed2
Mazda Bat <realmazdafan@gmail.com>
2025-12-16 17:00:05 -0700

    Load multiple files and show them in a loop.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 
 #include <filesystem>
+#include <vector>
+
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_timer.h"
 
 int main(int, char**) {
     SDL_Window* window;
@@ -31,35 +35,50 @@ int main(int, char**) {
             SDL_GetError());
     }
 
-    std::filesystem::path current_path = std::filesystem::current_path();
-    SDL_Log("Current path as string: %s", current_path.u8string().c_str());
+    // For old standers and GCCs, use boost in place of std
+    std::filesystem::path resource_path =
+        std::filesystem::current_path() / std::filesystem::path("res");
+    resource_path = std::filesystem::absolute(resource_path);
+    SDL_Log("Current resource path: %s", resource_path.u8string().c_str());
 
-    surface = SDL_LoadPNG("res/pic_sc.png");
-    if (!surface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-            "Couldn't create surface from image: %s",
-            SDL_GetError());
+    std::vector<SDL_Texture*> vector_textures;
+
+    for (const auto& entry : std::filesystem::directory_iterator(resource_path)) {
+        // extension name is not reliable - may need rely on the meatdata.
+        if (entry.path().extension() == ".png") {
+            SDL_Surface* surface = SDL_LoadPNG(entry.path().string().c_str());
+            if (!surface) continue;
+
+            SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+            SDL_DestroySurface(surface);
+
+            // texture is the slideshow
+            if (texture) vector_textures.push_back(texture);
+        }
     }
 
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-            "Couldn't create texture from surface: %s",
-            SDL_GetError());
-    }
-
-    SDL_DestroySurface(surface);
+    size_t current = 0;
+    Uint64 lastSwitch = SDL_GetTicks();
+    const Uint64 IMAGE_DELAY = 2000;
 
     while (1) {
         SDL_PollEvent(&event);
-        if (event.type == SDL_EVENT_QUIT)
-            break;
-        else if (event.type == SDL_EVENT_KEY_DOWN)
+        if (event.type == SDL_EVENT_QUIT)  // Windows's close button
             break;
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        else if (event.type == SDL_EVENT_KEY_DOWN)  // Any key down
+            break;
+
+        Uint64 now = SDL_GetTicks();
+        if (now - lastSwitch >= IMAGE_DELAY) {
+            // Play the slides in a loop
+            current = (current + 1) % vector_textures.size();
+            lastSwitch = now;
+        }
+
         SDL_RenderClear(renderer);
-        SDL_RenderTexture(renderer, texture, NULL, NULL);
+        SDL_RenderTexture(renderer, vector_textures[current], NULL, NULL);
         SDL_RenderPresent(renderer);
     }
 
